8de68956b0f119dd9dd3d722660a7d528cba6782
shimamoto <chibochibo@gmail.com>
2018-11-22 21:45:09 +0900

    save_model

--- python/pypio/pypio.py
+++ python/pypio/pypio.py
@@ -27,16 +27,42 @@ def find_events(app_name):
 
 
 def save_model(model):
-    engine = sc._jvm.org.apache.predictionio.e2.engine.PythonEngine
-    engine.model().set(model._to_java())
-    args = sys.argv
-#    args.append("--engine-factory")
-#    args.append(engine.getClass().getName())
-    main_args = utils.toJArray(sc._gateway, sc._gateway.jvm.String, args)
-    create_workflow = sc._jvm.org.apache.predictionio.workflow.CreateWorkflow
-    spark.stop()
-    create_workflow.main(main_args)
+    meta_storage = sc._jvm.org.apache.predictionio.data.storage.Storage.getMetaDataEngineInstances()
 
+    meta = sc._jvm.org.apache.predictionio.data.storage.EngineInstance.apply(
+        "",
+        "INIT", # status
+        sc._jvm.org.joda.time.DateTime.now(), # startTime
+        sc._jvm.org.joda.time.DateTime.now(), # endTime
+        "org.apache.predictionio.e2.engine.PythonEngine", # engineId - the value of engineFactory in engine.json
+        "1", # engineVersion - the hash of engine directory
+        "default", # engineVariant - the value of id in engine.json
+        "org.apache.predictionio.e2.engine.PythonEngine", # engineFactory
+        "", # batch
+        sc._jvm.scala.Predef.Map().empty(), # env
+        sc._jvm.scala.Predef.Map().empty(), # sparkConf
+        "", # dataSourceParams
+        "", # preparatorParams
+        "", # algorithmsParams
+        "" # servingParams
+    )
+    id = meta_storage.insert(meta)
+
+    kryo = sc._jvm.org.apache.predictionio.workflow.KryoInstantiator.newKryoInjection()
+    jl = sc._jvm.java.util.ArrayList()
+    jl.add(model._to_java())
+    data = sc._jvm.org.apache.predictionio.data.storage.Model(id, kryo.apply(sc._jvm.scala.collection.JavaConverters.asScalaBufferConverter(jl)))
+    model_storage = sc._jvm.org.apache.predictionio.data.storage.Storage.getModelDataModels()
+    model_storage.insert(data)
+
+    meta_storage.update(
+        sc._jvm.org.apache.predictionio.data.storage.EngineInstance.apply(
+            id, "COMPLETED", meta.startTime(), sc._jvm.org.joda.time.DateTime.now(),
+            meta.engineId(), meta.engineVersion(), meta.engineVariant(),
+            meta.engineFactory(), meta.batch(), meta.env(), meta.sparkConf(),
+            meta.dataSourceParams(), meta.preparatorParams(), meta.algorithmsParams(), meta.servingParams()
+        )
+    )
 
 def import_file(path, destination_frame=None, parse=True, header=None, sep=None, col_names=None, col_types=None,
                 na_strings=None, pattern=None):
