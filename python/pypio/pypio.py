957e72f6ac5de4be3dba5f922c95357bfd733334
shimamoto <chibochibo@gmail.com>
2018-12-03 19:55:37 +0900

    If app name is specified then create a new application

--- python/pypio/pypio.py
+++ python/pypio/pypio.py
@@ -12,7 +12,13 @@ from pypio.workflow import CleanupFunctions
 from pyspark.sql import SparkSession
 
 
-def init():
+def init(app_name=None):
+    """
+    Initialize pypio.
+    If app name is specified then create a new application.
+
+    :param app_name: app name. If specified, create a new app
+    """
     global spark
     spark = SparkSession.builder.getOrCreate()
     global sc
@@ -25,27 +31,43 @@ def init():
     cleanup_functions = CleanupFunctions(sqlContext)
     atexit.register(lambda: cleanup_functions.run())
     atexit.register(lambda: sc.stop())
+
+    if app_name is not None:
+        app = sc._jvm.org.apache.predictionio.tools.commands.App.create(app_name, sc._jvm.scala.Option.empty(), sc._jvm.scala.Option.empty(), "")
+        if app.isRight():
+            print("Created a new app:")
+            print("      Name: {}".format(app.right().get().app().name()))
+            print("        ID: {}".format(app.right().get().app().id()))
+            print("Access Key: {}".format(app.right().get().keys().head().key()))
     print("Initialized pypio")
 
 
 def find_events(app_name):
+    """
+    Returns a dataset of the specified app.
+
+    :param app_name: app name
+    :return: :py:class:`pyspark.sql.DataFrame`
+    """
     return p_event_store.find(app_name)
 
 
 def save_model(model, predict_columns):
+    """
+    Save a PipelineModel object to storage.
+
+    :param model: :py:class:`pyspark.ml.pipeline.PipelineModel`
+    :param predict_columns: prediction columns
+    :return: identifier for the trained model to use for predict
+    """
     if not predict_columns:
         raise ValueError("predict_columns should have more than one value")
-    serving_params = {"":{"columns":[]}}
-    serving_params[""]["columns"].extend(predict_columns)
-
     if os.environ.get('PYSPARK_PYTHON') is None:
         # spark-submit
         d = list_to_dict(sys.argv[1:])
-        engine_factory = d.get('--engine-factory')
         pio_env = list_to_dict([v for e in d['--env'].split(',') for v in e.split('=')])
     else:
         # pyspark
-        engine_factory = None
         pio_env = {k: v for k, v in os.environ.items() if k.startswith('PIO_')}
 
     meta_storage = sc._jvm.org.apache.predictionio.data.storage.Storage.getMetaDataEngineInstances()
@@ -55,17 +77,17 @@ def save_model(model, predict_columns):
         "INIT", # status
         sc._jvm.org.joda.time.DateTime.now(), # startTime
         sc._jvm.org.joda.time.DateTime.now(), # endTime
-        engine_factory or "org.apache.predictionio.e2.engine.PythonEngine", # engineId
+        "org.apache.predictionio.e2.engine.PythonEngine", # engineId
         "1", # engineVersion
         "default", # engineVariant
-        engine_factory or "org.apache.predictionio.e2.engine.PythonEngine", # engineFactory
+        "org.apache.predictionio.e2.engine.PythonEngine", # engineFactory
         "", # batch
         dict_to_scalamap(sc._jvm, pio_env), # env
         sc._jvm.scala.Predef.Map().empty(), # sparkConf
         "{\"\":{}}", # dataSourceParams
         "{\"\":{}}", # preparatorParams
         "[{\"default\":{}}]", # algorithmsParams
-        json.dumps(serving_params) # servingParams
+        json.dumps({"":{"columns":[v for v in predict_columns]}}) # servingParams
     )
     id = meta_storage.insert(meta)
 
